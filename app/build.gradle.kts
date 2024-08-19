c5a62e2cea937d6efc2b27f9e6eb6aea5c82843c
Zuanid-Sutlan <zunaidsultan786@gmail.com>
2024-08-19 14:35:44 +0500

    first preparation to release on play console (4.0/3.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.devStudio.todistap"
         minSdk = 23
         targetSdk = 34
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
